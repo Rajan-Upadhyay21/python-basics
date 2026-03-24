9541c34c3fdd1f26781c28f48e4ffaafefab243a
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 19:37:48 -0500

    Create function_with_return.py

---
+++ functions_advanced/function_with_return.py
@@ -0,0 +1,9 @@
+# Defining a function that returns a value
+def add_numbers(a, b):
+    return a + b
+
+# Calling the function and storing the result
+result = add_numbers(10, 20)
+
+# Printing the result
+print("Sum =", result)
