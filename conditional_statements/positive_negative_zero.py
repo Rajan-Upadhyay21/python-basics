5809137c569a1c077199f93af563f207213f159a
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 19:21:25 -0500

    Create positive_negative_zero.py

---
+++ conditional_statements/positive_negative_zero.py
@@ -0,0 +1,9 @@
+# Checking whether a number is positive, negative, or zero
+number = -5
+
+if number > 0:
+    print("Positive number")
+elif number < 0:
+    print("Negative number")
+else:
+    print("Zero")
