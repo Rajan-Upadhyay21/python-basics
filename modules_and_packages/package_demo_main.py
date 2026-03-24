89fb956937d098c35cfd2411b0b0b8b07502ce9d
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 20:32:34 -0500

    Create package_demo_main.py

---
+++ modules_and_packages/package_demo_main.py
@@ -0,0 +1,10 @@
+# ---------------------------------------------------------
+# Program: Package Demo Main File
+# Description:
+# This program demonstrates importing from a custom package.
+# ---------------------------------------------------------
+
+from my_package.operations import multiply, divide
+
+print("Multiplication:", multiply(6, 7))
+print("Division:", divide(20, 4))
