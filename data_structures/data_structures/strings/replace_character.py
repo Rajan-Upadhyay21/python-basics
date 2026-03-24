525887021f9511798e50a6afc42d944eb292e5fe
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-24 11:38:35 -0500

    Create replace_character.py

---
+++ data_structures/data_structures/strings/replace_character.py
@@ -0,0 +1,11 @@
+# ---------------------------------------------------------
+# Program: Replace Character
+# Description:
+# This program replaces a character in a string.
+# ---------------------------------------------------------
+
+text = "banana"
+updated_text = text.replace("a", "@")
+
+print("Original string:", text)
+print("Updated string:", updated_text)
