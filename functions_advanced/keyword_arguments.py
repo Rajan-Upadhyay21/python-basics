fc9e791803ca072cfc3048dfbbaa2cd128bd3524
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 19:38:18 -0500

    Create keyword_arguments.py

---
+++ functions_advanced/keyword_arguments.py
@@ -0,0 +1,8 @@
+# Defining a function with multiple parameters
+def student_info(name, age, city):
+    print("Name:", name)
+    print("Age:", age)
+    print("City:", city)
+
+# Calling the function using keyword arguments
+student_info(name="Rajan", age=23, city="Chicago")
