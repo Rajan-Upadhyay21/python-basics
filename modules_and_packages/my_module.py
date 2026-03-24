67dd4f412ea3466ee2e631131c8ffd598643bf1f
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 20:32:57 -0500

    Create my_module.py

---
+++ modules_and_packages/my_module.py
@@ -0,0 +1,10 @@
+# User-defined module with functions and variables
+
+app_name = "Python Learning Repository"
+version = "1.0"
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
