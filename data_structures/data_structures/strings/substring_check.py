44dfe6ddf454dd4d834d6cd559d78f21ddf40f22
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-24 11:38:16 -0500

    Create substring_check.py

---
+++ data_structures/data_structures/strings/substring_check.py
@@ -0,0 +1,13 @@
+# ---------------------------------------------------------
+# Program: Substring Check
+# Description:
+# This program checks if a substring exists in a string.
+# ---------------------------------------------------------
+
+text = "machine learning"
+substring = "learn"
+
+if substring in text:
+    print("Substring found.")
+else:
+    print("Substring not found.")
