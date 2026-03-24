66b60a4f65ba1526ae2d1b87e9badd2fce61bf59
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 19:22:57 -0500

    Create simple_login_check.py

---
+++ conditional_statements/simple_login_check.py
@@ -0,0 +1,8 @@
+# Simple username and password check
+username = "admin"
+password = "1234"
+
+if username == "admin" and password == "1234":
+    print("Login successful")
+else:
+    print("Invalid username or password")
