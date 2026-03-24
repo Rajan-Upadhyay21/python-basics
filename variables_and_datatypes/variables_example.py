5e3108f135c28e640a7692d14c492f7ed57ecfb0
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 19:10:41 -0500

    Create variables_example.py

---
+++ variables_and_datatypes/variables_example.py
@@ -0,0 +1,9 @@
+# Storing basic information in variables
+name = "Rajan"
+age = 23
+city = "Chicago"
+
+# Printing the values of the variables
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
