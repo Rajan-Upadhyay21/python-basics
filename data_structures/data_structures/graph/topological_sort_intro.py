68c5d5a893b9a74fe4768d190bc689f7b3c1bd93
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-24 13:57:07 -0500

    Create topological_sort_intro.py

---
+++ data_structures/data_structures/graph/topological_sort_intro.py
@@ -0,0 +1,41 @@
+# ---------------------------------------------------------
+# Program: Topological Sort
+# Description:
+# This program performs topological sorting on a DAG.
+# ---------------------------------------------------------
+
+from collections import deque
+
+def topological_sort(graph):
+    indegree = {node: 0 for node in graph}
+
+    for node in graph:
+        for neighbor in graph[node]:
+            indegree[neighbor] += 1
+
+    queue = deque([node for node in indegree if indegree[node] == 0])
+    order = []
+
+    while queue:
+        node = queue.popleft()
+        order.append(node)
+
+        for neighbor in graph[node]:
+            indegree[neighbor] -= 1
+            if indegree[neighbor] == 0:
+                queue.append(neighbor)
+
+    return order
+
+
+graph = {
+    5: [2, 0],
+    4: [0, 1],
+    2: [3],
+    3: [1],
+    1: [],
+    0: []
+}
+
+print("Topological order:")
+print(topological_sort(graph))
