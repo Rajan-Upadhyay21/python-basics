2d26f691f58df1349893421467794b651297e89b
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-24 11:34:52 -0500

    Create reverse_array.py

---
+++ data_structures/data_structures/arrays/reverse_array.py
@@ -0,0 +1,13 @@
+# ---------------------------------------------------------
+# Program: Reverse Array
+# Description:
+# This program reverses an array.
+# ---------------------------------------------------------
+
+arr = [1, 2, 3, 4, 5]
+
+print("Original array:", arr)
+
+reversed_arr = arr[::-1]
+
+print("Reversed array:", reversed_arr)
