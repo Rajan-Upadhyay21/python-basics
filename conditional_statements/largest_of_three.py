46b840319cd7954bbb03c6b62bb6b0a1cd512539
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 19:22:10 -0500

    Create largest_of_three.py

---
+++ conditional_statements/largest_of_three.py
@@ -0,0 +1,11 @@
+# Finding the largest number among three numbers
+a = 10
+b = 25
+c = 18
+
+if a >= b and a >= c:
+    print("a is the largest")
+elif b >= a and b >= c:
+    print("b is the largest")
+else:
+    print("c is the largest")
