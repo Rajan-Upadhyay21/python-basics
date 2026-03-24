861d2fbae2f26cf9c9066f41d27a719de95f5f7a
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 19:49:13 -0500

    Create sudoku_solver_recursive.py

---
+++ recursion/sudoku_solver_recursive.py
@@ -0,0 +1,88 @@
+# ---------------------------------------------------------
+# Program: Sudoku Solver using Recursion and Backtracking
+# Description:
+# This program solves a 9x9 Sudoku puzzle using recursion.
+# Empty cells are represented by 0.
+# ---------------------------------------------------------
+
+def print_board(board):
+    for row in board:
+        print(" ".join(str(num) for num in row))
+    print()
+
+
+def find_empty_location(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                return row, col
+    return None
+
+
+def is_valid(board, row, col, num):
+    # Check row
+    for x in range(9):
+        if board[row][x] == num:
+            return False
+
+    # Check column
+    for x in range(9):
+        if board[x][col] == num:
+            return False
+
+    # Check 3x3 subgrid
+    start_row = row - row % 3
+    start_col = col - col % 3
+
+    for i in range(3):
+        for j in range(3):
+            if board[start_row + i][start_col + j] == num:
+                return False
+
+    return True
+
+
+def solve_sudoku(board):
+    empty = find_empty_location(board)
+
+    # Base case: if no empty cell remains, puzzle is solved
+    if not empty:
+        return True
+
+    row, col = empty
+
+    # Try numbers 1 to 9
+    for num in range(1, 10):
+        if is_valid(board, row, col, num):
+            board[row][col] = num
+
+            if solve_sudoku(board):
+                return True
+
+            # Backtrack
+            board[row][col] = 0
+
+    return False
+
+
+# Sudoku puzzle
+sudoku_board = [
+    [3, 0, 6, 5, 0, 8, 4, 0, 0],
+    [5, 2, 0, 0, 0, 0, 0, 0, 0],
+    [0, 8, 7, 0, 0, 0, 0, 3, 1],
+    [0, 0, 3, 0, 1, 0, 0, 8, 0],
+    [9, 0, 0, 8, 6, 3, 0, 0, 5],
+    [0, 5, 0, 0, 9, 0, 6, 0, 0],
+    [1, 3, 0, 0, 0, 0, 2, 5, 0],
+    [0, 0, 0, 0, 0, 0, 0, 7, 4],
+    [0, 0, 5, 2, 0, 6, 3, 0, 0]
+]
+
+print("Original Sudoku Board:\n")
+print_board(sudoku_board)
+
+if solve_sudoku(sudoku_board):
+    print("Solved Sudoku Board:\n")
+    print_board(sudoku_board)
+else:
+    print("No solution exists for this Sudoku puzzle.")
