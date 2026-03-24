851d9a096f858eea454e9a7cd305bad992819c46
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-24 11:37:09 -0500

    Create string_traversal.py

---
+++ data_structures/data_structures/strings/string_traversal.py
@@ -0,0 +1,10 @@
+# ---------------------------------------------------------
+# Program: String Traversal
+# Description:
+# This program traverses a string character by character.
+# ---------------------------------------------------------
+
+text = "Python"
+
+for char in text:
+    print(char)
