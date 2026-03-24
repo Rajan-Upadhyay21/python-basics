887bcb1473d17c56c5677cdce8de1d03db91b530
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-24 11:33:23 -0500

    Create array_traversal.py

---
+++ data_structures/data_structures/arrays/array_traversal.py
@@ -0,0 +1,12 @@
+# ---------------------------------------------------------
+# Program: Array Traversal
+# Description:
+# This program demonstrates traversing an array and printing
+# all its elements one by one.
+# ---------------------------------------------------------
+
+arr = [10, 20, 30, 40, 50]
+
+print("Array elements are:")
+for element in arr:
+    print(element)
