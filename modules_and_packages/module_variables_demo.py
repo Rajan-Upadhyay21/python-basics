e56aa781e18b3fac608ea1a10c8df3717f84fb2c
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 20:32:19 -0500

    Create module_variables_demo.py

---
+++ modules_and_packages/module_variables_demo.py
@@ -0,0 +1,11 @@
+# ---------------------------------------------------------
+# Program: Module Variables Demo
+# Description:
+# This program demonstrates accessing variables from a
+# user-defined module.
+# ---------------------------------------------------------
+
+import my_module
+
+print("Application Name:", my_module.app_name)
+print("Version:", my_module.version)
