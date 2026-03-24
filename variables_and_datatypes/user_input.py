ab8c30514f68e43cac307444a9dc4f89898d101a
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 19:11:54 -0500

    Create user_input.py

---
+++ variables_and_datatypes/user_input.py
@@ -0,0 +1,7 @@
+# Taking user input for name and age
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+# Displaying the entered information
+print("Hello", name)
+print("You are", age, "years old.")
