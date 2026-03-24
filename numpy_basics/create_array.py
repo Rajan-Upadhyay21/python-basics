e2196a0ef83bc5b41e9937235db1916ea2b66fa7
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 20:35:36 -0500

    Create create_array.py

---
+++ numpy_basics/create_array.py
@@ -0,0 +1,22 @@
+# ---------------------------------------------------------
+# Program: Create NumPy Arrays
+# Description:
+# This program demonstrates different ways to create NumPy arrays.
+# ---------------------------------------------------------
+
+import numpy as np
+
+# Creating a 1D array
+array_1d = np.array([10, 20, 30, 40, 50])
+
+# Creating a 2D array
+array_2d = np.array([
+    [1, 2, 3],
+    [4, 5, 6]
+])
+
+print("1D Array:")
+print(array_1d)
+
+print("\n2D Array:")
+print(array_2d)
