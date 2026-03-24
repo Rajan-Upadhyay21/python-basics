e1b3a63101ca6ba398f528b01fc77c22df3316d9
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-24 11:37:25 -0500

    Create reverse_string.py

---
+++ data_structures/data_structures/strings/reverse_string.py
@@ -0,0 +1,11 @@
+# ---------------------------------------------------------
+# Program: Reverse String
+# Description:
+# This program reverses a string.
+# ---------------------------------------------------------
+
+text = "Python"
+reversed_text = text[::-1]
+
+print("Original string:", text)
+print("Reversed string:", reversed_text)
