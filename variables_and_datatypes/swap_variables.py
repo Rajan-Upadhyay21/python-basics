246e17893e955bff41ae99edd737f66a23976dd0
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 19:14:27 -0500

    Create swap_variables.py

---
+++ variables_and_datatypes/swap_variables.py
@@ -0,0 +1,16 @@
+# Assigning values to two variables
+a = 10
+b = 20
+
+# Printing before swapping
+print("Before swapping:")
+print("a =", a)
+print("b =", b)
+
+# Swapping values
+a, b = b, a
+
+# Printing after swapping
+print("After swapping:")
+print("a =", a)
+print("b =", b)
