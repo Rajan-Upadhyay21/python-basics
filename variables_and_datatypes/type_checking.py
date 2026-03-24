da6240d2412aad65e7b020ed531a9d32cc416cbe
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 19:11:20 -0500

    Create type_checking.py

---
+++ variables_and_datatypes/type_checking.py
@@ -0,0 +1,9 @@
+# Creating variables with different data types
+a = 25
+b = 3.14
+c = "Hello"
+
+# Checking and printing the type of each variable
+print("Type of a:", type(a))
+print("Type of b:", type(b))
+print("Type of c:", type(c))
