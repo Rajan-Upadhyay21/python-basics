5c04b1f7da79901968df597e5a1f5be0d5b0c836
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 19:39:05 -0500

    Create nested_function.py

---
+++ functions_advanced/nested_function.py
@@ -0,0 +1,11 @@
+# Defining a nested function
+def outer_function():
+    print("This is the outer function")
+
+    def inner_function():
+        print("This is the inner function")
+
+    inner_function()
+
+# Calling the outer function
+outer_function()
