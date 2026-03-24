71bb62099ec885b27bf763d1f61a0ce42d94f77b
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 20:31:12 -0500

    Create import_datetime_module.py

---
+++ modules_and_packages/import_datetime_module.py
@@ -0,0 +1,14 @@
+# ---------------------------------------------------------
+# Program: Import Datetime Module
+# Description:
+# This program demonstrates the use of the datetime module.
+# ---------------------------------------------------------
+
+import datetime
+
+current_date_time = datetime.datetime.now()
+
+print("Current date and time:", current_date_time)
+print("Current year:", current_date_time.year)
+print("Current month:", current_date_time.month)
+print("Current day:", current_date_time.day)
