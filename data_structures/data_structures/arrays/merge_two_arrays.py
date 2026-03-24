0b76581480b39601fbbfe8c159e2e1c800f3d705
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-24 11:35:09 -0500

    Create merge_two_arrays.py

---
+++ data_structures/data_structures/arrays/merge_two_arrays.py
@@ -0,0 +1,14 @@
+# ---------------------------------------------------------
+# Program: Merge Two Arrays
+# Description:
+# This program merges two arrays into one.
+# ---------------------------------------------------------
+
+arr1 = [1, 2, 3]
+arr2 = [4, 5, 6]
+
+merged_array = arr1 + arr2
+
+print("First array:", arr1)
+print("Second array:", arr2)
+print("Merged array:", merged_array)
