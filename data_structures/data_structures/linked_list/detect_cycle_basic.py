370ec2087d738e3d2e6b7eb1071de02d61b45676
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-24 11:43:34 -0500

    Create detect_cycle_basic.py

---
+++ data_structures/data_structures/linked_list/detect_cycle_basic.py
@@ -0,0 +1,42 @@
+# ---------------------------------------------------------
+# Program: Detect Cycle in Linked List
+# Description:
+# This program detects whether a linked list contains a cycle
+# using slow and fast pointers.
+# ---------------------------------------------------------
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def detect_cycle(head):
+    slow = head
+    fast = head
+
+    while fast is not None and fast.next is not None:
+        slow = slow.next
+        fast = fast.next.next
+
+        if slow == fast:
+            return True
+
+    return False
+
+
+# Creating linked list with cycle
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+node4 = Node(4)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node2   # cycle created here
+
+if detect_cycle(node1):
+    print("Cycle detected in linked list.")
+else:
+    print("No cycle detected.")
