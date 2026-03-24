829350e589f1c0e788064aa753be115a149d05e6
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 19:22:26 -0500

    Create check_leap_year.py

---
+++ conditional_statements/check_leap_year.py
@@ -0,0 +1,7 @@
+# Checking whether a year is a leap year
+year = 2024
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(year, "is a leap year")
+else:
+    print(year, "is not a leap year")
