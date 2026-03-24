6706b1538006b8291faaf56acbf51f99c378cc80
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 19:40:35 -0500

    Create docstring_example.py

---
+++ functions_advanced/docstring_example.py
@@ -0,0 +1,8 @@
+# Defining a function with a docstring
+def multiply(a, b):
+    """This function returns the product of two numbers."""
+    return a * b
+
+# Printing the result
+print(multiply(4, 5))
+print(multiply.__doc__)
