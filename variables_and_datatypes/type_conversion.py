0db11d35e712e25c326097701c5fe9fb0f417885
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 19:14:10 -0500

    Create type_conversion.py

---
+++ variables_and_datatypes/type_conversion.py
@@ -0,0 +1,9 @@
+# Converting one data type into another
+num = "25"
+
+# Converting string to integer
+converted_num = int(num)
+
+# Printing both values and their types
+print("Original value:", num, "Type:", type(num))
+print("Converted value:", converted_num, "Type:", type(converted_num))
