837ebaba0a0248a5496eaedfe81870162d521783
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 19:39:35 -0500

    Create check_even_odd_function.py

---
+++ functions_advanced/check_even_odd_function.py
@@ -0,0 +1,8 @@
+# Defining a function to check even or odd
+def check_even_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    return "Odd"
+
+# Calling the function
+print(check_even_odd(8))
