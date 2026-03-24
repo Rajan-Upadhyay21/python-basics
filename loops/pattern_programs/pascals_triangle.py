9cf59ec7785132aac7c3d0151cc905c8044972db
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 19:34:21 -0500

    Create pascals_triangle.py

---
+++ loops/pattern_programs/pascals_triangle.py
@@ -0,0 +1,10 @@
+# Printing Pascal's triangle
+rows = 5
+
+for i in range(rows):
+    num = 1
+    print(" " * (rows - i), end="")
+    for j in range(i + 1):
+        print(num, end=" ")
+        num = num * (i - j) // (j + 1) if j < i else 1
+    print()
