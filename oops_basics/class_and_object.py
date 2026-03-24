672347da0c7622b87750268c9ee7076b529e04b6
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 20:20:08 -0500

    Create class_and_object.py

---
+++ oops_basics/class_and_object.py
@@ -0,0 +1,17 @@
+# ---------------------------------------------------------
+# Program: Class and Object Example
+# Description:
+# This program demonstrates how to create a class and
+# create objects from that class.
+# ---------------------------------------------------------
+
+class Student:
+    def display_details(self):
+        print("This is a student object.")
+
+
+# Creating object
+student1 = Student()
+
+# Calling method using object
+student1.display_details()
