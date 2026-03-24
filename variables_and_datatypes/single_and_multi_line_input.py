97e4c57580c5744715ec31f696f6e64a92411443
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 19:14:43 -0500

    Create single_and_multi_line_input.py

---
+++ variables_and_datatypes/single_and_multi_line_input.py
@@ -0,0 +1,6 @@
+# Taking two inputs in a single line
+x, y = input("Enter two numbers separated by space: ").split()
+
+# Printing the entered values
+print("First value:", x)
+print("Second value:", y)
