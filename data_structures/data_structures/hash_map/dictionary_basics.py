d7884944e515d6021f3fb4dc4b914f625711b38c
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-24 11:59:15 -0500

    Create dictionary_basics.py

---
+++ data_structures/data_structures/hash_map/dictionary_basics.py
@@ -0,0 +1,20 @@
+# ---------------------------------------------------------
+# Program: Dictionary Basics
+# Description:
+# This program demonstrates basic dictionary creation
+# and access in Python.
+# ---------------------------------------------------------
+
+student = {
+    "name": "Rajan",
+    "age": 23,
+    "course": "Computer Science"
+}
+
+print("Dictionary:")
+print(student)
+
+print("\nAccessing values:")
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("Course:", student["course"])
