205c7fe9e0373c21e086daead2e5fc505acbe054
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 19:40:05 -0500

    Create function_for_area_of_circle.py

---
+++ functions_advanced/function_for_area_of_circle.py
@@ -0,0 +1,9 @@
+# Importing math module
+import math
+
+# Defining a function to calculate area of a circle
+def area_of_circle(radius):
+    return math.pi * radius * radius
+
+# Calling the function
+print("Area =", area_of_circle(5))
