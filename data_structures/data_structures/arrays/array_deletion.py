1db719ed97d283e96df08ee12dc661cec20efe06
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-24 11:33:58 -0500

    Create array_deletion.py

---
+++ data_structures/data_structures/arrays/array_deletion.py
@@ -0,0 +1,13 @@
+# ---------------------------------------------------------
+# Program: Array Deletion
+# Description:
+# This program demonstrates deleting an element from an array.
+# ---------------------------------------------------------
+
+arr = [10, 20, 30, 40, 50]
+
+print("Original array:", arr)
+
+arr.remove(30)
+
+print("Array after deletion:", arr)
