cd3232438b29f3cb354bc24c993fd9506adecf55
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 20:31:45 -0500

    Create from_import_example.py

---
+++ modules_and_packages/from_import_example.py
@@ -0,0 +1,11 @@
+# ---------------------------------------------------------
+# Program: From-Import Example
+# Description:
+# This program demonstrates importing specific functions
+# from a module.
+# ---------------------------------------------------------
+
+from math import sqrt, factorial
+
+print("Square root of 81:", sqrt(81))
+print("Factorial of 6:", factorial(6))
