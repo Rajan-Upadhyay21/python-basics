3ca957c1b6478c56eb3ffcc63994a69db00473ec
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-24 11:33:41 -0500

    Create array_insertion.py

---
+++ data_structures/data_structures/arrays/array_insertion.py
@@ -0,0 +1,13 @@
+# ---------------------------------------------------------
+# Program: Array Insertion
+# Description:
+# This program demonstrates inserting an element into an array.
+# ---------------------------------------------------------
+
+arr = [10, 20, 30, 40]
+
+print("Original array:", arr)
+
+arr.append(50)
+
+print("Array after insertion:", arr)
