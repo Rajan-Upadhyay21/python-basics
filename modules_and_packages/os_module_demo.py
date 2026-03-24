48d5ef63cad2847b48c339ae78f23154feada05b
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 20:33:18 -0500

    Create os_module_demo.py

---
+++ modules_and_packages/os_module_demo.py
@@ -0,0 +1,11 @@
+# ---------------------------------------------------------
+# Program: OS Module Demo
+# Description:
+# This program demonstrates basic usage of the os module.
+# ---------------------------------------------------------
+
+import os
+
+print("Current working directory:", os.getcwd())
+print("List of files and folders:")
+print(os.listdir())
