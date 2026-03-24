db3e08bd30522c49f8ceebda4d92490920fc417d
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 19:11:01 -0500

    Create integer_float_string.py

---
+++ variables_and_datatypes/integer_float_string.py
@@ -0,0 +1,9 @@
+# Creating variables of different data types
+integer_value = 10
+float_value = 10.5
+string_value = "Python"
+
+# Printing each variable
+print("Integer:", integer_value)
+print("Float:", float_value)
+print("String:", string_value)
