eea94a696f6f4184b43e5ae9e89083bfdd8d664d
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 20:30:36 -0500

    Create import_math_module.py

---
+++ modules_and_packages/import_math_module.py
@@ -0,0 +1,15 @@
+# ---------------------------------------------------------
+# Program: Import Math Module
+# Description:
+# This program demonstrates the use of Python's built-in
+# math module.
+# ---------------------------------------------------------
+
+import math
+
+number = 25
+
+print("Square root:", math.sqrt(number))
+print("Power:", math.pow(5, 2))
+print("Ceiling:", math.ceil(4.2))
+print("Floor:", math.floor(4.8))
