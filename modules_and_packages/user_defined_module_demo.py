2b92b311eaa85a26594abbba0460170c40b56960
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 20:32:00 -0500

    Create user_defined_module_demo.py

---
+++ modules_and_packages/user_defined_module_demo.py
@@ -0,0 +1,10 @@
+# ---------------------------------------------------------
+# Program: User-Defined Module Demo
+# Description:
+# This program imports functions from a user-defined module.
+# ---------------------------------------------------------
+
+import my_module
+
+print("Addition:", my_module.add(10, 20))
+print("Subtraction:", my_module.subtract(20, 5))
