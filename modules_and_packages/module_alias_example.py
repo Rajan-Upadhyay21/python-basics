5a95697515b8f708b94f3fb1f52407506833fc89
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-23 20:31:28 -0500

    Create module_alias_example.py

---
+++ modules_and_packages/module_alias_example.py
@@ -0,0 +1,11 @@
+# ---------------------------------------------------------
+# Program: Module Alias Example
+# Description:
+# This program demonstrates importing a module with an alias.
+# ---------------------------------------------------------
+
+import math as m
+
+print("Value of pi:", m.pi)
+print("Factorial of 5:", m.factorial(5))
+print("Cosine of 0:", m.cos(0))
