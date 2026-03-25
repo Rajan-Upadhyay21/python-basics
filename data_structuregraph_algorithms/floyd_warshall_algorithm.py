eb1164114b9423a5d6a7a864809e351b8fc72bcf
Rajan Upadhyay <rajanupadhyay2106@gmail.com>
2026-03-25 15:55:38 -0500

    Create floyd_warshall_algorithm.py

---
+++ data_structuregraph_algorithms/floyd_warshall_algorithm.py
@@ -0,0 +1,34 @@
+# ---------------------------------------------------------
+# Program: Floyd-Warshall Algorithm
+# Description:
+# This program finds shortest paths between all pairs
+# of vertices in a weighted graph.
+# ---------------------------------------------------------
+
+def floyd_warshall(graph):
+    n = len(graph)
+    dist = [row[:] for row in graph]
+
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if dist[i][k] + dist[k][j] < dist[i][j]:
+                    dist[i][j] = dist[i][k] + dist[k][j]
+
+    return dist
+
+
+INF = float("inf")
+
+graph = [
+    [0, 3, INF, 7],
+    [8, 0, 2, INF],
+    [5, INF, 0, 1],
+    [2, INF, INF, 0]
+]
+
+result = floyd_warshall(graph)
+
+print("All-pairs shortest path matrix:")
+for row in result:
+    print(row)
